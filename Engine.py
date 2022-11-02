65cdfd1a9cd84c14916a0b043fff8d345228194c
Rafael De La Cruz <41638598+RafaCruz22@users.noreply.github.com>
2022-11-01 21:11:45 -0400

    not much changed just added a call to UI.cellDeactivate when user has lost or won game

--- Engine.py
+++ Engine.py
@@ -1,5 +1,5 @@
 from Cell import *
-from InputOutput import *
+from UserInterface import *
 from random import randint
 
 
@@ -15,13 +15,13 @@ class Engine():
 
         self.field = [[Cell() for j in range(size)] for i in range(size)]
 
+        self.size = size
         self.numOfMines = NumOfMines
-        self.minesMarked = None
 
         self.placeMine(size)  # place mine on board
 
-        # initialize input and output
-        self.out = InputOutput(
+        # initialize input and UIput
+        self.UI = UserInterface(
             self.field,
             self.numOfMines,
             width=size,
@@ -34,12 +34,12 @@ class Engine():
 
             x = randint(0, size-1)
             y = randint(0, size-1)
-
+            
             if self.field[x][y] == '0':
                 self.field[x][y].putMine()  # put the mine
-                counter += 1  # increase the counter for the mines
+                counter += 1                # increase mines count
 
-                # adjust the numbers of the neighboring slots
+                # adjust the adjucency values of the neighboring slots
                 self.adjustCounters(x, y)
 
     def adjustCounters(self, x: int, y: int) -> None:
@@ -76,37 +76,39 @@ class Engine():
 
     def playGame(self) -> None:
         """ runs the game """
-
-        self.out.intro()
-        self.out.infoBar()  # shows the information bar
-        self.out.createBoard()  # creates the minesweeper board
-        self.out.printBoard()  # print board to console
+        self.UI.introWindow()  # created window with intro message
+        self.UI.gameBar()      # shows the information bar
+        self.UI.createBoard()  # creates the minesweeper board
+        self.UI.printBoard()   # print board to console
 
         while True:
+            if self.UI.gameEnd:
+                self.resetMinesandCells()
 
-            result = self.playRound()  # play a round, get its result
+            mine = self.playRound()          # play a round, get its result
+                
+            markedMines = abs((int(self.UI.currentMines.get()) - self.numOfMines))
 
-            if not result:  # we opened a bomb!
-                self.out.killTimer()
-                # self.out.announce("lost")  # window with defeat message
-                break  # end game
-
-            else:
-                # player wins if flags are on mines or all nonmine boxes are revealed
-                self.minesMarked = abs(
-                    (int(self.out.currentMines.get()) - self.numOfMines))
-
-                if self.minesMarked == self.numOfMines:  # all bombs found!
-                    self.out.killTimer()
-                    # self.out.announce('player')  # window with victory message
-                    break  # end game
-
-        self.out.showFullBoard()  # reveal all mine cells
-
-        # # keeps main window from closing out
-        self.out.board.wait_window(window=self.out.master)
+            if mine or markedMines == self.numOfMines:
+                self.UI.killTimer()
+                    
+                # disable clicking on the board.
+                self.UI.cellDeactivate()
+                self.UI.revealAllMines()  # reveal all mine cells
 
     def playRound(self) -> bool:
         """ plays one round
-        post: returns False if user has not clicked on mine, True otherwise"""
-        return False if self.out.getInput() == '9' else True
+        post: returns True if user has clicked on mine, False otherwise"""
+        return True if self.UI.cellValue() == '9' else False
+
+    def resetMinesandCells(self) -> None:
+        try:
+            # self.UI.board.unbind("<Button-1>")
+            self.field.clear()
+            self.UI.mineField.clear()
+            self.field = self.UI.mineField = [[Cell() for j in range(self.size)] for i in range(self.size)]
+            self.UI.gameEnd = False
+            self.placeMine(self.size)  # place mine on board
+    
+        except:
+            print('\n--> reset not successful \nSomething went wrong')
