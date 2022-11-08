6bd95587bc036dfcf14d40a8633034f3837e94a5
Rafael De La Cruz <41638598+RafaCruz22@users.noreply.github.com>
2022-11-08 08:49:11 -0500

    was trying some things but nothing panned out. shouldn't have any changes

--- Minesweeper.py
+++ Minesweeper.py
@@ -4,6 +4,7 @@ import Engine as game
 def main() -> None:
     mineSweeper = game.Engine(10, 10)
     mineSweeper.playGame()
+    # mineSweeper.UI.master.mainloop()
 
 if __name__ == "__main__":
     main()
